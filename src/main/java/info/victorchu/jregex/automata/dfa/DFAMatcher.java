ec01a20b2daf513cae1061fe8e6caf80199a62b9
chutian <victorchu0610@outlook.com>
2023-09-21 16:42:58 +0800

    NFA -> DFA

---
+++ src/main/java/info/victorchu/jregex/automata/dfa/DFAMatcher.java
@@ -0,0 +1,10 @@
+package info.victorchu.jregex.automata.dfa;
+
+/**
+ * DFA 匹配器
+ *
+ * @author victorchu
+ */
+public class DFAMatcher
+{
+}
---
+++ src/main/java/info/victorchu/jregex/automata/nfa/NFAMatcher.java
@@ -0,0 +1,11 @@
+package info.victorchu.jregex.automata.nfa;
+
+/**
+ * NFA 匹配器
+ *
+ * @author victorchu
+ */
+public class NFAMatcher
+{
+
+}
